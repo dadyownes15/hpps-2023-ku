6137ad3a47873a4f89637afe8b27edacdd7c666c
Frederik H N <112876551+FREDDYJPEG@users.noreply.github.com>
2023-12-14 15:17:07 +0100

    Update bruteforce.c

--- a3-handout/bruteforce.c
+++ a3-handout/bruteforce.c
@@ -5,5 +5,15 @@
 
 int* knn(int k, int d, int n, const double *points, const double* query) {
   assert(0);
+  int* closest = (int*)malloc(k * sizeof(int));
+  for (int i = 0; i < k; ++i) {
+    closest[i] = -1;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    insert_if_closer(k, d, points, closest, query, i);
+  }
+
+  return closest;
 }
 
